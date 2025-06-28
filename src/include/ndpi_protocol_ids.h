5fb37548f7524cc6efe81df274528912a2bbabbb
eazi201 <kfablionel@gmail.com>
2025-06-28 20:36:52 +0200

    Add  Blacknut ,Boosteroid and Rumble protocol(SNI detection WIP)

--- src/include/ndpi_protocol_ids.h
+++ src/include/ndpi_protocol_ids.h
@@ -481,6 +481,9 @@ typedef enum {
   NDPI_PROTOCOL_KICK                  = 450,
   NDPI_PROTOCOL_HAMACHI               = 451,
   NDPI_PROTOCOL_GLBP                  = 452,
+  NDPI_PROTOCOL_BLACKNUT              = 453,
+  NDPI_PROTOCOL_BOOSTEROID            = 454,
+  NDPI_PROTOCOL_RUMBLE                = 455,
 
 #ifdef CUSTOM_NDPI_PROTOCOLS
 #include "../../../nDPI-custom/custom_ndpi_protocol_ids.h"
--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -2914,6 +2914,22 @@ static void init_protocol_defaults(struct ndpi_detection_module_struct *ndpi_str
                           ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0) /* TCP */,
                           ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */,
                           0);
+  ndpi_set_proto_defaults(ndpi_str, 1 /* cleartext */, 0 /* nw proto */, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_BLACKNUT,
+              "Blacknut", NDPI_PROTOCOL_CATEGORY_STREAMING, NDPI_PROTOCOL_QOE_CATEGORY_UNSPECIFIED,
+              ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0) /* TCP */,
+              ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */,
+              0);
+  ndpi_set_proto_defaults(ndpi_str, 1 /* cleartext */, 0 /* nw proto */, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_BOOSTEROID,
+              "Boosteroid", NDPI_PROTOCOL_CATEGORY_STREAMING, NDPI_PROTOCOL_QOE_CATEGORY_UNSPECIFIED,
+              ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0),
+              ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0),
+              0);
+
+  ndpi_set_proto_defaults(ndpi_str, 1 /* cleartext */, 0 /* nw proto */, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_RUMBLE,
+              "Rumble", NDPI_PROTOCOL_CATEGORY_STREAMING, NDPI_PROTOCOL_QOE_CATEGORY_UNSPECIFIED,
+              ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0),
+              ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0),
+              0);
   
 #ifdef CUSTOM_NDPI_PROTOCOLS
 #include "../../../nDPI-custom/custom_ndpi_main.c"
